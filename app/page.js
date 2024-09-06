7295511045dba788ddda0726a6de6401c946f344
Maxime Duhamel <max.duh22@gmail.com>
2024-09-06 05:56:35 +0200

    added page and layout

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import '@/app/ui/global.css'
+import { inter, lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata = {
+  title: {
+    template: '%s | Maxime Duhamel',
+    default: 'Portfolio',
+  },
+  description: 'Portfolio by Maxime Duhamel.',
+};
+
+export default function RootLayout({children}) {
+  return (
+    <html lang="en">
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
