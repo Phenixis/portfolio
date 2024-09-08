7a34c28e014c840573cda8e13bb18e9bfa58763e
Maxime Duhamel <max.duh22@gmail.com>
2024-09-08 02:13:06 +0200

    added a notification badges and changed bg color

--- app/ui/Mastodon-logo.js
+++ app/ui/Mastodon-logo.js
@@ -9,19 +9,23 @@ export default function MastodonLogo() {
 
     return (
         <div className="h-full content-center">
-            <a
-                target="_blank"
-                rel="me"
-                href="https://bzh.social/@mduhamel"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-            >
-                {isHovered ? (
-                    <PiMastodonLogoFill className="size-7" />
-                ) : (
-                    <PiMastodonLogo className="size-7" />
-                )}
-            </a>
+            <div className="flex flex-row-reverse">
+                <a
+                    target="_blank"
+                    rel="me"
+                    href="https://bzh.social/@mduhamel"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                    >
+                    {isHovered ? (
+                        <PiMastodonLogoFill className="size-8" />
+                    ) : (
+                        <PiMastodonLogo className="size-8" />
+                    )}
+                </a>
+                <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-sky-400 opacity-75"></span>
+                <span class="absolute inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+            </div>
         </div>
     );
 }
