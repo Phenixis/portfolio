5a2d253f067b76053603f5ba066d20baa17ba46a
Maxime Duhamel <max.duh22@gmail.com>
2024-09-08 13:58:08 +0200

    changed Contact Button

--- app/ui/Contact.js
+++ app/ui/Contact.js
@@ -1,13 +1,11 @@
-'use client'
 
 export default function ContactMe() {
     return (
         <div>
             <div className="h-full content-center">
                 <button
-                    className="text-lg p-2 rounded-xl shadow-lg shadow-black-700 font-bold border-black border-2 duration-200 hover:shadow-inner"
-                >
-                    Contact Me
+                    className="text-lg p-2 rounded-xl shadow-xl shadow-black-700 font-bold border-black border-2 duration-200 hover:shadow-inner">
+                        Contact Me
                 </button>
             </div>
         </div>
