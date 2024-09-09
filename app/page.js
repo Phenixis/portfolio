73b12552d9d2dc07adc1ad4ef1fa80a88fc5a370
Maxime Duhamel <max.duh22@gmail.com>
2024-09-09 06:23:36 +0200

    added the quote,. explanation and change the theme

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
-import { DiGithubBadge } from "react-icons/di";
-import { LuConstruction } from "react-icons/lu";
 import LinkedInLogo from '/app/ui/LinkedIn-logo';
 import GithubLogo from '/app/ui/Github-logo';
 import MastodonLogo from '/app/ui/Mastodon-logo';
 import XLogo from '/app/ui/X-logo';
 import ContactMe from '/app/ui/Contact';
 import GithubCalendar from '/app/ui/github-calendar';
+import Quote from '/app/ui/quote';
 
 export const metadata = {
     title: "Portfolio",
@@ -25,6 +24,13 @@ export default function Page() {
                 <ContactMe />
             </header>
             <main className="m-8 flex flex-col items-center">
+                <p className="text-md text-center">
+                    Someone I admire for his achievements once said :
+                </p>
+                <Quote quote="Be impatient with your inputs, and patient with your outputs." author="Alex Hormozi" />
+                <p className="text-md text-center">
+                    This is why my GitHub activity is so important to me. I try to make a commit every day, even if it's just a small one.
+                </p>
                 <GithubCalendar />
             </main>
         </main>
--- app/ui/github-calendar.js
+++ app/ui/github-calendar.js
@@ -1,15 +1,21 @@
 'use client'
 
 import GitHubCalendar from 'react-github-calendar';
+import React from 'react';
 
 export default function GithubCalendar() {
+    const theme = {
+        light: ['#ece0d1', '#dbc1ac', '#967259', '#634832', '#38220f']
+    };
+
     return (
-        <div className="bg-gray-50 p-4 rounded-lg border-2 border-gray-700">
-        <GitHubCalendar
-            username="Phenixis"
-            hideTotalCount={true}
-            hideColorLegend={true}
-        />
+        <div className="bg-amber-50 p-4 rounded-lg">
+            <GitHubCalendar
+                username="Phenixis"
+                hideTotalCount={true}
+                hideColorLegend={true}
+                theme={theme}
+            />
         </div>
     );
 }
