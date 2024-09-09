4fcd6d433d09a5da2da8c86d5932889ea57c2b08
Maxime Duhamel <max.duh22@gmail.com>
2024-09-09 22:02:55 +0200

    changed header

--- app/ui/components/header.js
+++ app/ui/components/header.js
@@ -1,14 +1,14 @@
-import LinkedInLogo from '/app/ui/LinkedIn-logo';
-import GithubLogo from '/app/ui/Github-logo';
-import MastodonLogo from '/app/ui/Mastodon-logo';
-import XLogo from '/app/ui/X-logo';
-import ContactMe from '/app/ui/Contact';
+import LinkedInLogo from '/app/ui/logos/LinkedIn-logo';
+import GithubLogo from '/app/ui/logos/Github-logo';
+import MastodonLogo from '/app/ui/logos/Mastodon-logo';
+import XLogo from '/app/ui/logos/X-logo';
 
 export default function Header() {
     return (
-        <header className="flex justify-between p-2 sticky top-2 w-full bg-amber-50 rounded-lg">
-            <ContactMe />
-            <p className='text-5xl block p-3 rounded-xl shadow-lg shadow-black-700 font-bold'>Maxime Duhamel</p>
+        <header className="flex justify-between p-2 sticky top-5 w-full bg-amber-50 rounded-lg shadow-lg shadow-black-700">
+            <p className='text-2xl block p-3 rounded-xl font-bold'>
+                Maxime Duhamel
+            </p>
             <div className="flex justify-around space-x-0.5">
                 <XLogo />
                 <GithubLogo />
