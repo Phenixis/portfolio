fd890819a251db67612e798c2ab121978d8db23e
Maxime Duhamel <max.duh22@gmail.com>
2024-09-10 05:53:44 +0200

    Added images and moved board and quote

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import GithubCalendar from '/app/ui/github-calendar';
 import Quote from '/app/ui/quote';
 import Header from '/app/ui/components/header';
+import Image from 'next/image';
 
 export const metadata = {
     title: "Portfolio",
@@ -10,12 +11,17 @@ export default function Page() {
     return (
         <main className="m-12 h-screen">
             <Header />
-            <main className="m-8 flex flex-col items-center">
-                <Quote quote="Be impatient with actions and patient with results." author="Naval Ravikant" />
-                <p className="text-md text-center font-semibold">
-                    This is why my GitHub activity is so important to me. I try to make a commit every day, even if it's just a small one.
-                </p>
-                <GithubCalendar />
+            <main className="m-8 flex flex-col items-center w-full">
+                <div className='flex justify-around w-full h-max space-x-4'>
+                    <div className='flex flex-col items-end justify-between'>
+                        <Quote quote="Be impatient with your inputs and patient with your outputs." author="Alex Hormozi" />
+                        <Image src="/the_principle.png" alt="Le Principe" width={200} height={90} />
+                    </div>
+                    <div className='flex flex-col items-start justify-between'>
+                        <GithubCalendar />
+                        <Image src="/the_actions.png" alt="Le Principe" width={200} height={92}/>
+                    </div>
+                </div>
             </main>
         </main>
     )
