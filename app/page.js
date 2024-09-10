5c5c9f5372972fc823494734af6d1d21307869f3
Maxime Duhamel <max.duh22@gmail.com>
2024-09-10 19:24:18 +0200

    moved some pages and changed responsive

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import GithubCalendar from '/app/ui/github-calendar';
-import Quote from '/app/ui/quote';
+import GithubCalendar from '/app/ui/components/github-calendar';
+import Quote from '/app/ui/components/quote';
 import Header from '/app/ui/components/header';
 import Image from 'next/image';
 
@@ -12,7 +12,7 @@ export default function Page() {
         <main className="m-4 h-screen md:m-12">
             <Header />
             <main className="mt-6 md:m-8 flex flex-col items-center w-full">
-                <div className='flex flex-col justify-around w-full h-max md:space-x-4 md:flex-row'>
+                <div className='flex flex-col justify-around w-full h-max md:space-y-2 md:space-x-4 md:flex-row'>
                     <div className='flex flex-col items-start space-y-1 md:items-end md:justify-between'>
                         <Quote quote="Be impatient with your inputs and patient with your outputs." author="Alex Hormozi" />
                         <Image src="/the_principle.png" alt="Le Principe" width={200} height={90} />
