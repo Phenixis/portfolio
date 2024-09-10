cc8e2713b830711be37ea791b4c9e32241c00149
Maxime Duhamel <max.duh22@gmail.com>
2024-09-10 17:43:41 +0200

    test on header

--- app/ui/components/header.js
+++ app/ui/components/header.js
@@ -1,14 +1,37 @@
+'use client'
+
 import LinkedInLogo from '/app/ui/logos/LinkedIn-logo';
 import GithubLogo from '/app/ui/logos/Github-logo';
 import MastodonLogo from '/app/ui/logos/Mastodon-logo';
 import XLogo from '/app/ui/logos/X-logo';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className="flex justify-between p-2 sticky top-5 w-full bg-amber-50 rounded-lg shadow-lg shadow-black-700">
-            <p className='text-2xl block p-3 rounded-xl font-bold'>
-                Maxime Duhamel
-            </p>
+        <header
+            className={`flex justify-between p-2 sticky top-5 w-full bg-amber-50 rounded-lg shadow-lg shadow-black-700 ${
+                isScrolled ? 'scale-75' : ''
+            }`}
+        >
+            <p className="text-2xl block p-3 rounded-xl font-bold">Maxime Duhamel</p>
             <div className="flex justify-around space-x-0.5">
                 <XLogo />
                 <GithubLogo />
@@ -16,5 +39,5 @@ export default function Header() {
                 <MastodonLogo />
             </div>
         </header>
-    )
+    );
 }
