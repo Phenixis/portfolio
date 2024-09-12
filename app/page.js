89bbfad5ffd462694f5a72724ff0742cfa00d0c4
Maxime Duhamel <max.duh22@gmail.com>
2024-09-12 06:55:45 +0200

    Refactor Page component to improve responsivity and design

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Page() {
     return (
         <main className="m-4 h-screen md:m-12">
             <Header />
-            <main className="mt-6 md:mt-8 flex flex-col items-center w-full">
+            <main className="mt-6 md:mt-8 flex flex-col items-center w-full space-y-6">
                 <div className='flex flex-col justify-around w-full h-max md:space-y-2 md:space-x-4'>
                     <div className='flex flex-col md:flex-row'>
                         <Quote quote="Be impatient with your inputs and patient with your outputs." author="Alex Hormozi" />
@@ -30,18 +30,20 @@ export default function Page() {
                 </div>
                 <div className='min-w-7/12 max-w-full'>
                     <div className='flex flex-col items-center w-full'>
-                        <Image className="rounded-full" src="/pdp.jpg" alt="A picture of me" width={200} height={200}/>
+                    <div className="avatar">
+                        <div className="w-48 rounded-full">
+                            <img src="pdp.jpg" />
+                        </div>
+                    </div>
                         <div className='w-full'>
                             <p className="text-lg font-semibold lg:text-2xl">Hello! I'm Maxime Duhamel</p>
                             <p className='lg:text-lg'>
                                 I'm a computer science student at the <a href="https://iut-lannion.univ-rennes.fr/" title="the UIT website" className='underline'>University Institute of Technology in Lannion.</a> I'm specialized in the administration, the management and the exploitation of data.
                             </p>
                             <div className='flex justify-end lg:text-lg'>
-                                <p className='font-bold p-2 rounded-lg shadow-lg shadow-black-700'>
-                                    <a href="https://www.canva.com/design/DAFuEEd36CM/BbgwPzNszoiUH_AkGxfYPQ/view?utm_content=DAFuEEd36CM&utm_campaign=designshare&utm_medium=link&utm_source=editor" target='_blank' title="My resumee" className='underline'>
-                                        Get a copy of my resumee &gt;
-                                    </a>
-                                </p>
+                                <a href="https://www.canva.com/design/DAFuEEd36CM/BbgwPzNszoiUH_AkGxfYPQ/view?utm_content=DAFuEEd36CM&utm_campaign=designshare&utm_medium=link&utm_source=editor" target='_blank'>
+                                    <button className="btn btn-primary">Get a copy of my resumee &gt;</button>
+                                </a>
                             </div>
                         </div>
                     </div>
