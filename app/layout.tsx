bc6be5945ea0dd3e61aea52b2468cd2cdeb3d3a9
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-15 17:06:46 +0100

    fix: refactor RootLayout component for improved structure and readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,58 +9,59 @@ import Footer from './components/footer'
 import { baseUrl } from './sitemap'
 
 export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: 'Maxime Duhamel',
-    template: '%s | Maxime Duhamel',
-  },
-  description: 'This is my portfolio.',
-  openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
-    url: baseUrl,
-    siteName: 'My Portfolio',
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+    metadataBase: new URL(baseUrl),
+    title: {
+        default: 'Maxime Duhamel',
+        template: '%s | Maxime Duhamel',
     },
-  },
+    description: 'This is my portfolio.',
+    openGraph: {
+        title: 'My Portfolio',
+        description: 'This is my portfolio.',
+        url: baseUrl,
+        siteName: 'My Portfolio',
+        locale: 'en_US',
+        type: 'website',
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    },
 }
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes: string[]) => classes.filter(Boolean).join(' ')
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html
-      lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
-          {children}
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
-        </main>
-      </body>
-    </html>
-  )
+    return (
+        <html
+            lang="en"
+        >
+            <body className={cx(
+                'antialiased max-w-xl px-4 text-black bg-white dark:text-white dark:bg-black lg:mx-auto',
+                GeistSans.variable,
+                GeistMono.variable
+            )}>
+                <main className="flex flex-col justify-between min-w-0 min-h-screen pt-6 px-2 md:px-0">
+                    <div className="flex flex-col">
+                        <Navbar />
+                        {children}
+                    </div>
+                    <Footer />
+                    <Analytics />
+                    <SpeedInsights />
+                </main>
+            </body>
+        </html>
+    )
 }
