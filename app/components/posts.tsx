7807367840d6ecfc692842686b6afd2bda74d0c8
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-15 15:10:12 +0100

    fix: change BlogPosts component to default export and suppress hydration warning for date formatting

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { formatDate, getBlogPosts } from 'app/blog/utils'
 
-export function BlogPosts() {
+export default function BlogPosts() {
   let allBlogs = getBlogPosts()
 
   return (
@@ -22,7 +22,7 @@ export function BlogPosts() {
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums" suppressHydrationWarning>
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
