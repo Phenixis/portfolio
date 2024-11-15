00efe6dc1214fea6f7c5eb8169ca3c80c58dcee8
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-15 20:57:33 +0100

    fix: update post link styling for improved responsiveness and layout consistency

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,11 +18,11 @@ export default function BlogPosts() {
                 .map((post) => (
                     <Link
                         key={post.slug}
-                        className="flex flex-col space-y-1 mb-4 p-1 rounded border border-transparent hover:border-neutral-200 dark:hover:border-neutral-500"
+                        className="flex flex-col space-y-1 mb-4 p-1 rounded border border-neutral-200 md:border-transparent hover:border-neutral-200 dark:hover:border-neutral-500 "
                         href={`/blog/${post.slug}`}
                     >
                         <div className="w-full flex flex-col md:items-center md:flex-row space-x-0 md:space-x-2">
-                            <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+                            <p className="text-neutral-600 dark:text-neutral-400 w-full md:w-[100px] tabular-nums">
                                 {formatDate(post.metadata.publishedAt, false)}
                             </p>
                             <div>
