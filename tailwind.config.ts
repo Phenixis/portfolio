aa80856fef5158d659f4c8d9ad38b0997432d26a
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-15 17:06:27 +0100

    fix: migrate PostCSS configuration to .mjs format and add Tailwind CSS typography plugin

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  darkMode: ['class'],
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+};
+export default config;
