bbc773db5cd249766e8a68fa874503bddb74f873
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-16 10:51:49 +0100

    feat: create new projects page component

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Page() {
+    return (
+        <section className="page">
+            <h1 className="page-title">
+                My Projects
+            </h1>
+        </section>
+    )
+}
