3f1c5d7ba737e6829aa4720d7acf3af92f762901
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-16 22:43:13 +0100

    fix: add hover effect to logo component for improved interactivity

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -4,7 +4,7 @@ import * as ListIconDark from 'public/listIconDark.png';
 
 export default function Logo({ className, size=16 } : { className?: string, size?: number }) {
     return (
-        <div className={"duration-1000 group-hover:rotate-45 " + className}>
+        <div className={"duration-1000 group-hover:rotate-45 hover:rotate-45" + className}>
             <Image src={ListIconLight} width={size} height={size} alt="list-style" className="dark:hidden" />
             <Image src={ListIconDark} width={size} height={size} alt="list-style" className="hidden dark:block" />
         </div>
