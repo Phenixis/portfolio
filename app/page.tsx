ef7a56c729d626970d50b87db1ae5a6094de3a59
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-16 10:51:06 +0100

    style: update page component styles for improved readability and structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import BlogPosts from 'app/components/posts'
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+    <section className="page">
+      <h1 className="page-title">
         My Portfolio
       </h1>
-      <p className="mb-4 text-justify text-neutral-600 dark:text-neutral-300 leading-[1.8]">
+      <p className="page-description">
         I'm Maxime, a 19 years old french student in computer science. I love building useful applications and websites. I'm currently learning React, Next.js and Tailwindcss by building <a href="https://www.wisecart.app" target='_blank' className="underline decoration-dashed hover:text-black dark:hover:text-white" >WiseCart</a>, a shopping list builder using a meal-oriented approach.
       </p>
       <div className="my-8">
