71b25022518a2f895330bc6b886d5325104b696f
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-02-08 14:40:12 +0100

    Adding "my story"

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,15 @@
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { ChevronDown } from "lucide-react"
 
 
 export default function Page() {
@@ -6,17 +18,39 @@ export default function Page() {
             <h1 className="page-title">
                 My Projects
             </h1>
-            <p>
-                All my projects share a common goal :
-                <ul className="list-decorated">
-                    <li>
-                        <Image src={listStyle} width={16} height={16} alt="list-style" />
+            <Collapsible className="group/collapsible duration-1000 data-[state=open]:bg-gray-100 border-transparent hover:bg-gray-100 rounded-md p-2">
+                <CollapsibleTrigger className="flex justify-between gap-4">
+                    <p className="text-left">
+                        I build web applications since Septembre 2024, but my journey started a long time ago...
+                    </p>
+                    <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                </CollapsibleTrigger>
+                <CollapsibleContent className="flex flex-col space-y-4">
+                    <p className="mt-4">
+                        My first ever "technological invention" was born in 2021. I was using the  Google Drive application to synchronize a large number of files (1000~2000) between my computer and my phone.
+                    </p>
+                    <p>
+                        With such number of files, errors happen and Google Drive displayed them one by one for each file. There was no option for ignoring every errors, so it was mandatory to delete them one. by. one.
+                    </p>
+                    <p>
+                        As I was learning how to code in Python, I decided that I would build a small program that would make it for me. There was no API so I needed to use the graphical interface. I searched for a library to move the mouse to click on the 3 dots button, click on "Delete" and start again for the next error.
+                    </p>
+                    <TooltipProvider>
                         <p>
-                            to make me write regularly
+                            It took me probably a full afternoon to build the program from A to Z, but I had a problem, so I built something to solve it. Since then, the
+                            <Tooltip>
+                                <TooltipTrigger className="underline cursor-default">
+                                    producer mindset
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    In opposition with the "consumer" mindset, producters build the solution to their problems instead of buying them.
+                                </TooltipContent>
+                            </Tooltip> had a little place in a corner of my mind.
                         </p>
-                    </li>
-                </ul>
-            </p>
+                    </TooltipProvider>
+                </CollapsibleContent>
+            </Collapsible>
+
         </section>
     )
 }
