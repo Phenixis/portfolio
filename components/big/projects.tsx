126ff52269e75c3ad39c82ce80480eda8e546eb0
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-02-08 16:48:28 +0100

    feat: add Projects component to display a grid of project details

---
+++ components/big/projects.tsx
@@ -0,0 +1,11 @@
+import Project from "./project";
+
+export default function Projects() {
+    return (
+        <div className="grid grid-cols-2 gap-2">
+            <Project name="Wisecart" description="The perfect app to make your shopping list in seconds." color="green" link="https://wisecart.app" state="Discontinued"/>
+            <Project name="Boilerplate.md" description="The best open-source boilerplate for NextJS." color="blue" link="https://boilerplate.maximeduhamel.com" state="Running" />
+            <Project name="ScriptAI" description="Your writer assistant specialized in storytelling" color="red" link="https://boilerplate.maximeduhamel.com" state="Building" />
+        </div>
+    )
+}
