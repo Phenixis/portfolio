2719dc1a971b1e6bc0fa86c6b630b15bee873b39
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-02-12 19:26:11 +0100

    fix: change logo link to use inline-block for better layout

--- components/big/logo.tsx
+++ components/big/logo.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 export default function Logo({ className, size=16 } : { className?: string, size?: number }) {
     return (
-        <Link href="/" className={'duration-1000 group-hover/Logo:rotate-45 ' + className}>
+        <Link href="/" className={'inline-block duration-1000 group-hover/Logo:rotate-45 ' + className}>
             <Image src={ListIconLight} width={size} height={size} alt="list-style" className="dark:hidden" />
             <Image src={ListIconDark} width={size} height={size} alt="list-style" className="hidden dark:block" />
         </Link>
