cb806121110023563fd93e76a0f8f433aba21041
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-11 06:32:35 +0100

    feat: integrate InputOTP component for enhanced login experience

--- app/(back-office)/login/page.tsx
+++ app/(back-office)/login/page.tsx
@@ -1,12 +1,31 @@
-import { setSession } from "@/lib/auth/session"
+"use client";
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSeparator,
+    InputOTPSlot,
+} from "@/components/ui/input-otp"
 
 export default function Login() {
-    setSession()
-
     return (
-        <div>
-            <h1>Login</h1>
-            <p>Sign in to access the back office</p>
+        <div className="w-full min-h-screen flex items-center justify-center">
+                <InputOTP maxLength={4}>
+                    <InputOTPGroup>
+                        <InputOTPSlot index={0} />
+                    </InputOTPGroup>
+                    <InputOTPSeparator />
+                    <InputOTPGroup>
+                        <InputOTPSlot index={1} />
+                    </InputOTPGroup>
+                    <InputOTPSeparator />
+                    <InputOTPGroup>
+                        <InputOTPSlot index={2} />
+                    </InputOTPGroup>
+                    <InputOTPSeparator />
+                    <InputOTPGroup>
+                        <InputOTPSlot index={3} />
+                    </InputOTPGroup>
+                </InputOTP>
         </div>
     )
 }
