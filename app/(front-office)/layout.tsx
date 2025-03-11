1f82cd896ef7f43d868b2207ef0f04a7d0a1cf3c
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-11 05:57:25 +0100

    feat: implement layout components for front-office and back-office, add dashboard page

--- app/(front-office)/layout.tsx
+++ app/(front-office)/layout.tsx
@@ -1,73 +1,13 @@
-import '../global.css'
-import type { Metadata } from 'next'
-import { baseUrl } from '../sitemap'
-import { Domine, Geist_Mono, Ubuntu_Sans_Mono } from 'next/font/google';
-import Main from '../../components/big/main'
+import Main from '@/components/big/main'
 
-const domine = Domine({
-    subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-    subsets: ['latin'],
-})
-
-const ubuntuSansMono = Ubuntu_Sans_Mono({
-    subsets: ['latin'],
-})
-
-export const metadata: Metadata = {
-    metadataBase: new URL(baseUrl),
-    title: {
-        default: 'Maxime Duhamel',
-        template: '%s | Maxime Duhamel',
-    },
-    description: 'This is my portfolio.',
-    openGraph: {
-        title: 'My Portfolio',
-        description: 'This is my portfolio.',
-        url: baseUrl,
-        siteName: 'My Portfolio',
-        locale: 'en_US',
-        type: 'website',
-    },
-    robots: {
-        index: true,
-        follow: true,
-        googleBot: {
-            index: true,
-            follow: true,
-            'max-video-preview': -1,
-            'max-image-preview': 'large',
-            'max-snippet': -1,
-        },
-    },
-}
-
-const cx = (...classes: string[]) => classes.filter(Boolean).join(' ')
-
-export default function RootLayout({
+export default function FrontOfficeLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
     return (
-        <html
-            lang="en"
-        >
-            <head>
-                <link rel="icon" href="/favicon.png" sizes='any' />
-            </head>
-            <body className={cx(
-                'antialiased max-w-3xl text-black bg-white dark:text-white dark:bg-black md:mx-auto',
-                domine.className,
-                geistMono.className,
-                ubuntuSansMono.className,
-            )}>
-                <Main>
-                    {children}
-                </Main>
-            </body>
-        </html>
+        <Main>
+            {children}
+        </Main>
     )
 }
