7193cf3d67c00af8b386049670946e24645c3d11
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-11 05:44:36 +0100

    add CRUD operations for todo items in database queries

---
+++ lib/db/queries.ts
@@ -0,0 +1,76 @@
+import { desc, and, eq, isNull, or, isNotNull, count, sql } from 'drizzle-orm';
+import { db } from './drizzle';
+import { todo } from './schema';
+
+// # TODO
+
+// ## Create
+export async function createTodo(title: string) {
+    return await db.insert(todo).values({ title });
+}
+
+// ## Read
+export async function getTodos() {
+    return await db.select().from(todo).orderBy(desc(todo.created_at));
+}
+
+export async function getTodoById(id: number) {
+    return await db.select().from(todo).where(eq(todo.id, id));
+}
+
+export async function getCompletedTodos() {
+    return await db.select().from(todo).where(isNotNull(todo.completed_at)).orderBy(desc(todo.completed_at));
+}
+
+export async function getUncompletedTodos() {
+    return await db.select().from(todo).where(isNull(todo.completed_at)).orderBy(desc(todo.created_at));
+}
+
+export async function searchTodosByTitle(title: string) {
+    return await db.select().from(todo).where(sql`${todo.title} LIKE ${`%${title}%`}`);
+}
+
+// ## Update
+
+export async function updateTodo(id: number, title: string, done: boolean) {
+    const already_done = await db.select({
+        done: count().as('done')
+    }).from(todo).where(and(eq(todo.id, id), isNotNull(todo.completed_at)));
+
+    return await db.update(todo).set({
+        title: title,
+        completed_at: done ? (already_done[0].done > 0 ? todo.completed_at : sql`CURRENT_TIMESTAMP`) : null,
+        updated_at: sql`CURRENT_TIMESTAMP`
+    }).where(eq(todo.id, id));
+}
+
+export async function updateTodoTitle(id: number, title: string) {
+    return await db.update(todo).set({
+        title: title,
+        updated_at: sql`CURRENT_TIMESTAMP`
+    }).where(eq(todo.id, id));
+}
+
+export async function markTodoAsDone(id: number) {
+    const already_done = await db.select({
+        done: count().as('done')
+    }).from(todo).where(and(eq(todo.id, id), isNotNull(todo.completed_at)));
+
+    return await db.update(todo).set({
+        completed_at: already_done[0].done > 0 ? todo.completed_at : sql`CURRENT_TIMESTAMP`,
+        updated_at: sql`CURRENT_TIMESTAMP`
+    }).where(eq(todo.id, id));
+}
+
+export async function markTodoAsUndone(id: number) {
+    return await db.update(todo).set({
+        completed_at: null,
+        updated_at: sql`CURRENT_TIMESTAMP`
+    }).where(eq(todo.id, id));
+}
+
+// ## Delete
+
+export async function deleteTodoById(id: number) {
+    return await db.delete(todo).where(eq(todo.id, id));
+}
