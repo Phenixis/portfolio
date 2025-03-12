ce954c7ebbaedb6a59f4bbb15ce6616d4b43f7dd
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-12 07:30:56 +0100

    feat: implement optimistic toggling for TodoDisplay component

--- components/big/todos/todoDisplay.tsx
+++ components/big/todos/todoDisplay.tsx
@@ -1,22 +1,49 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useOptimistic, startTransition } from "react";
 import { Skeleton } from "@/components/ui/skeleton"
 import { type Todo } from "@/lib/db/schema"
+import { toggleTodo } from "@/lib/db/queries";
 
 export default function TodoDisplay({ todo }: { todo?: Todo }) {
-    const [isToggled, setIsToggled] = useState(false);
+    const [isToggled, setIsToggled] = useState(todo ? todo.completed_at !== null : false);
+    async function toggle(todo: Todo) {
+        startTransition(() => {
+            toggleOptimistic(isToggled);
+        });
+
+        try {
+            await toggleTodo(todo.id, isToggled);
+
+            setIsToggled(!isToggled);
+        } catch (error) {
+            startTransition(() => {
+                toggleOptimistic(isToggled);
+            });
+        }
+    }
+
+    const [optimisticState, toggleOptimistic] = useOptimistic(
+        isToggled,
+        (prev) => (
+            !prev
+        )
+    )
 
     return (
         <>
             <input type="checkbox" className="hidden" name="taskButton" id="taskButton" onClick={
-                () => setIsToggled(!isToggled)
+                () => {
+                    if (todo) {
+                        toggle(todo)
+                    }
+                }
             } />
             <label htmlFor="taskButton" className="flex space-x-1 items-center group">
                 {
                     todo ? (
-                        <div className={`relative p-2 size-1 border border-neutral rounded-300 ${isToggled ? 'bg-primary' : ''}`}>
-                            <div className={`absolute inset-0 w-1/2 h-1/2 z-20 m-auto duration-300 ${isToggled ? 'lg:group-hover:bg-background' : 'lg:group-hover:bg-primary'}`} />
+                        <div className={`relative p-2 size-1 border border-neutral rounded-300 ${optimisticState ? 'bg-primary' : ''}`}>
+                            <div className={`absolute inset-0 w-1/2 h-1/2 z-20 m-auto duration-300 ${optimisticState ? 'lg:group-hover:bg-background' : 'lg:group-hover:bg-primary'}`} />
                         </div>
                     ) : (
                         <Skeleton className="w-5 h-5" />
@@ -24,7 +51,7 @@ export default function TodoDisplay({ todo }: { todo?: Todo }) {
                 }
                 {
                     todo ? (
-                        <p className={`${isToggled ? 'line-through' : ''}`}>
+                        <p className={`${optimisticState ? 'line-through' : ''}`}>
                             {todo.title}
                         </p>
                     ) : (
