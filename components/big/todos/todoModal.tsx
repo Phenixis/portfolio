706d404ab7e06a28ac13d663a0d9aaba32377e2c
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-12 12:18:07 +0100

    feat: add importance, urgency, and duration fields to todo modal

--- components/big/todos/todoModal.tsx
+++ components/big/todos/todoModal.tsx
@@ -38,14 +38,34 @@ export function TodoModal({ className, todo }: { className?: string, todo?: Todo
                         }
                     </DialogTitle>
                 </DialogHeader>
-                    <form className="space-y-4">
+                <form className="space-y-4">
+                    <div>
+                        <Label htmlFor="title">
+                            Title
+                        </Label>
+                        <Input type="text" name="title" defaultValue={todo ? todo.title : ''} />
+                    </div>
+                    <div className="flex space-x-4">
                         <div>
-                            <Label htmlFor="title">
-                                Title
+                            <Label htmlFor="importance">
+                                Importance
                             </Label>
-                            <Input type="text" name="title" defaultValue={todo ? todo.title : ''} />
+                            <Input type="number" name="importance" defaultValue={todo ? todo.importance : 0} min={0} max={5} />
                         </div>
-                    </form>
+                        <div>
+                            <Label htmlFor="urgency">
+                                Urgency
+                            </Label>
+                            <Input type="text" name="urgency" defaultValue={todo ? todo.urgency : 0} min={0} max={5} />
+                        </div>
+                        <div>
+                            <Label htmlFor="duration">
+                                Duration
+                            </Label>
+                            <Input type="text" name="duration" defaultValue={todo ? todo.duration : 0} min={0} max={3} />
+                        </div>
+                    </div>
+                </form>
                 <DialogFooter className="flex justify-end">
                     <Button type="submit">
                         {
