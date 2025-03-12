62d94c63e36f87a1b2109590784ac45f3e79f709
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-12 09:18:48 +0100

    feat: add TodosCard component for displaying todos with modal interaction

---
+++ components/big/todos/todosCard.tsx
@@ -0,0 +1,31 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import Todos from "@/components/big/todos/todos"
+import { TodoModal } from "@/components/big/todos/todoModal"
+import { cn } from "@/lib/utils"
+
+export function TodosCard({
+    className
+}: {
+    className: string
+}) {
+    return (
+        <Card className={cn(`w-full max-w-xl group/TodoCard`, className)}>
+            <CardHeader className="flex flex-row items-center justify-between">
+                <CardTitle>Todos</CardTitle>
+                <TodoModal className="opacity-0 duration-300 group-hover/TodoCard:opacity-100" />
+            </CardHeader>
+            <CardContent>
+                <CardDescription>
+                    <Todos />
+                </CardDescription>
+            </CardContent>
+        </Card>
+    )
+}
