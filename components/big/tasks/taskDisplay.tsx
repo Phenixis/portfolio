22349e617c84c999d1a5cadb54d84f5b8dc1fbf1
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-12 06:49:26 +0100

    feat: enhance TaskDisplay component with loading skeletons for empty states

--- components/big/tasks/taskDisplay.tsx
+++ components/big/tasks/taskDisplay.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function TaskDisplay({ title }: { title?: string }) {
     const [isToggled, setIsToggled] = useState(false);
@@ -11,16 +12,25 @@ export default function TaskDisplay({ title }: { title?: string }) {
                 () => setIsToggled(!isToggled)
             } />
             <label htmlFor="taskButton" className="flex space-x-1 items-center group">
-                <div className={`relative p-2 my-1 size-1 border border-neutral rounded-300 ${isToggled ? 'bg-primary' : ''}`}>
-                    <div className={`absolute inset-0 w-1/2 h-1/2 z-20 m-auto duration-300 ${isToggled ? 'lg:group-hover:bg-background' : 'lg:group-hover:bg-primary'}`} />
-                </div>
-                <div>
-                    <p className={`${isToggled ? 'line-through' : ''}`}>
-                        {
-                            title ? title : "Task"
-                        }
-                    </p>
-                </div>
+                {
+                    title ? (
+
+                        <div className={`relative p-2 size-1 border border-neutral rounded-300 ${isToggled ? 'bg-primary' : ''}`}>
+                            <div className={`absolute inset-0 w-1/2 h-1/2 z-20 m-auto duration-300 ${isToggled ? 'lg:group-hover:bg-background' : 'lg:group-hover:bg-primary'}`} />
+                        </div>
+                    ) : (
+                        <Skeleton className="w-5 h-5" />
+                    )
+                }
+                {
+                    title ? (
+                        <p className={`${isToggled ? 'line-through' : ''}`}>
+                            {title}
+                        </p>
+                    ) : (
+                        <Skeleton className="w-full h-4" />
+                    )
+                }
             </label>
         </>
     )
