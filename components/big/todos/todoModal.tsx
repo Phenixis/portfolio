b7241d0025745c6d4c6c3376270f93235867bb4d
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-12 19:23:22 +0100

    feat: enhance TodoModal with form handling and keyboard shortcuts for submission

--- components/big/todos/todoModal.tsx
+++ components/big/todos/todoModal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     Dialog,
     DialogContent,
@@ -9,14 +11,72 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { type Todo } from "@/lib/db/schema"
-import { PlusIcon, PenIcon } from "lucide-react"
+import { type Todo } from "@/lib/db/schema";
+import { createTodo, updateTodo } from "@/lib/db/queries";
+import { PlusIcon, PenIcon, Loader } from "lucide-react";
+import { startTransition, useActionState, useRef, useEffect, useState } from "react";
+import { ActionState } from "@/middleware";
 
 export function TodoModal({ className, todo }: { className?: string, todo?: Todo }) {
     const mode = todo ? 'edit' : 'create';
+    const formRef = useRef<HTMLFormElement>(null)
+    const [open, setOpen] = useState(false);
+
+    async function formFunction(state: ActionState, form: FormData) {
+        try {
+            let result = null;
+
+            if (mode === 'edit') {
+                result = await updateTodo(parseInt(form.get('id') as string), form.get('title') as string, parseInt(form.get('importance') as string), parseInt(form.get('urgency') as string), parseInt(form.get('duration') as string));
+            } else {
+                result = await createTodo(form.get('title') as string, parseInt(form.get('importance') as string), parseInt(form.get('urgency') as string), parseInt(form.get('duration') as string));
+            }
+
+            if (result) {
+                setOpen(false);
+                return { success: "Mis à jour" }
+            }
+            return { error: "Erreur lors de la mise à jour" }
+        } catch (error) {
+            return { error: "" + error }
+        }
+    }
+
+    const [state, formAction, pending] = useActionState<ActionState, FormData>(
+        formFunction,
+        { error: "" }
+    );
+
+    useEffect(() => {
+        const handleKeyPress = (event: KeyboardEvent) => {
+            if (event.ctrlKey && event.key === "Enter") {
+                startTransition(() => {
+                    if (formRef.current) {
+                        formAction(new FormData(formRef.current))
+                    }
+                })
+            }
+        }
+
+        document.addEventListener("keydown", handleKeyPress)
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyPress)
+        }
+    }, [formAction])
+
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        startTransition(() => {
+            if (formRef.current) {
+                formAction(new FormData(formRef.current))
+            }
+        })
+    }
 
     return (
-        <Dialog>
+        <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger className={className}>
                 {
                     mode === 'edit' ? (
@@ -38,7 +98,8 @@ export function TodoModal({ className, todo }: { className?: string, todo?: Todo
                         }
                     </DialogTitle>
                 </DialogHeader>
-                <form className="space-y-4">
+                <form ref={formRef} onSubmit={handleSubmit} className="space-y-4">
+                    <input type="text" name="id" value={todo ? todo.id : ''} hidden readOnly />
                     <div>
                         <Label htmlFor="title">
                             Title
@@ -65,18 +126,25 @@ export function TodoModal({ className, todo }: { className?: string, todo?: Todo
                             <Input type="text" name="duration" defaultValue={todo ? todo.duration : 0} min={0} max={3} />
                         </div>
                     </div>
-                </form>
-                <DialogFooter className="flex justify-end">
-                    <Button type="submit">
+                    <DialogFooter className="flex flex-row items-center justify-between">
                         {
-                            mode === 'edit' ? (
-                                'Save'
+                            pending ? (
+                                <Loader className="animate-spin size-4" />
                             ) : (
-                                'Create'
+                                state?.error && <p className="text-red-500">{state.error}</p>
                             )
                         }
-                    </Button>
-                </DialogFooter>
+                        <Button type="submit">
+                            {
+                                mode === 'edit' ? (
+                                    'Save'
+                                ) : (
+                                    'Create'
+                                )
+                            }
+                        </Button>
+                    </DialogFooter>
+                </form>
             </DialogContent>
         </Dialog>
     )
