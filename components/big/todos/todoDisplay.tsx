421742e086a584afe2bc0833da267e7dc8a3758b
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-12 09:18:44 +0100

    feat: integrate TodoModal for enhanced todo display and interaction

--- components/big/todos/todoDisplay.tsx
+++ components/big/todos/todoDisplay.tsx
@@ -4,6 +4,7 @@ import { useState, useOptimistic, startTransition } from "react";
 import { Skeleton } from "@/components/ui/skeleton"
 import { type Todo } from "@/lib/db/schema"
 import { toggleTodo } from "@/lib/db/queries";
+import { TodoModal } from "./todoModal";
 
 export default function TodoDisplay({ todo }: { todo?: Todo }) {
     const [isToggled, setIsToggled] = useState(todo ? todo.completed_at !== null : false);
@@ -39,23 +40,25 @@ export default function TodoDisplay({ todo }: { todo?: Todo }) {
                     }
                 }
             } />
-            <label htmlFor="taskButton" className="flex space-x-1 items-center group">
+            <label htmlFor="taskButton" className="flex justify-between group/todo p-1 duration-300 hover:bg-primary/5">
                 {
                     todo ? (
-                        <div className={`relative p-2 size-1 border border-neutral rounded-300 ${optimisticState ? 'bg-primary' : ''}`}>
-                            <div className={`absolute inset-0 w-1/2 h-1/2 z-20 m-auto duration-300 ${optimisticState ? 'lg:group-hover:bg-background' : 'lg:group-hover:bg-primary'}`} />
-                        </div>
+                        <>
+                            <div className="flex space-x-1 items-center">
+                                <div className={`relative p-2 size-1 border border-neutral rounded-300 ${optimisticState ? 'bg-primary' : ''}`}>
+                                    <div className={`absolute inset-0 w-1/2 h-1/2 z-20 m-auto duration-300 ${optimisticState ? 'lg:group-hover/todo:bg-background' : 'lg:group-hover/todo:bg-primary'}`} />
+                                </div>
+                                <p className={`${optimisticState ? 'line-through' : ''}`}>
+                                    {todo.title}
+                                </p>
+                            </div>
+                            <TodoModal className="duration-300 opacity-0 group-hover/todo:opacity-100" todo={todo} />
+                        </>
                     ) : (
-                        <Skeleton className="w-5 h-5" />
-                    )
-                }
-                {
-                    todo ? (
-                        <p className={`${optimisticState ? 'line-through' : ''}`}>
-                            {todo.title}
-                        </p>
-                    ) : (
-                        <Skeleton className="w-full h-4" />
+                        <>
+                            <Skeleton className="w-5 h-5" />
+                            <Skeleton className="w-full h-4" />
+                        </>
                     )
                 }
             </label>
