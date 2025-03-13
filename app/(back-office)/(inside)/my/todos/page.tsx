2da5d5d24dd8e30bf0789130f4477acc245070ca
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-13 06:28:50 +0100

    feat: integrate AverageCompletionTime chart into TodosPage layout for enhanced task tracking

--- app/(back-office)/(inside)/my/todos/page.tsx
+++ app/(back-office)/(inside)/my/todos/page.tsx
@@ -1,11 +1,12 @@
 import { TodosCard } from "@/components/big/todos/todosCard"
+import AverageCompletionTime from "@/components/big/todos/charts/averageCompletionTime"
 
 export default function TodosPage() {
     return (
         <div className="h-full flex flex-col space-y-4">
-            <div className="w-full h-1/2 flex space-x-4">
-                <TodosCard orderBy="score" limit={5} />
-                <div className="bg-gray-500 h-full w-full" />
+            <div className="w-full h-1/2 flex flex-col lg:flex-row space-x-4">
+                <TodosCard className="max-w-full lg:max-w-xl" orderBy="score" limit={5} />
+                <AverageCompletionTime />
             </div>
             <div className="w-full h-1/2 flex space-x-4">
                 <TodosCard className="max-w-full" orderBy="importance" />
