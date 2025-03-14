0e69655b1701678d9899baed3b998bf144bde4bd
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-14 09:55:28 +0100

    Remove try/catch because error are handle higher

--- app/(api)/api/weather/route.ts
+++ app/(api)/api/weather/route.ts
@@ -6,18 +6,15 @@ export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const lat = searchParams.get("lat");
     const lon = searchParams.get("lon");
-    try {
-        const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPENWEATHERMAP_API_KEY}`;
-        const result = await fetch(url);
+    
+    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPENWEATHERMAP_API_KEY}`;
+    const result = await fetch(url);
 
-        if (!result.ok) {
-            throw new Error("Failed to fetch weather data");
-        }
-
-        const data = await result.json();
-
-        return NextResponse.json(data);
-    } catch (error) {
-        return NextResponse.json({ error: "Failed to fetch weather data: " + (error as Error).message }, { status: 500 });
+    if (!result.ok) {
+        throw new Error("Failed to fetch weather data");
     }
+
+    const data = await result.json();
+
+    return NextResponse.json(data);
 }
