d69b03ba7973d5699507935a86505aa73d19336b
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-23 17:49:21 +0100

    refactor TodoDisplay component: center-align project badge

--- components/big/todos/todoDisplay.tsx
+++ components/big/todos/todoDisplay.tsx
@@ -149,7 +149,7 @@ export default function TodoDisplay({ todo, orderedBy }: { todo?: (Todo | Todo &
 						</div>
 						<div className="flex items-center space-x-2">
 							{"project" in todo && todo.project && (
-								<Badge className="mr-2" variant="outline">
+								<Badge className="mr-2 text-center" variant="outline">
 									{todo.project.title}
 								</Badge>
 							)}
