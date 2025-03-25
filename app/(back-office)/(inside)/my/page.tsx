5a299b5783762257240bd27a388387c5f57627d8
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-25 10:29:08 +0100

    remove unnecessary gap from DashboardPage layout

--- app/(back-office)/(inside)/my/page.tsx
+++ app/(back-office)/(inside)/my/page.tsx
@@ -5,7 +5,7 @@ import { TodosCard } from "@/components/big/todos/todosCard"
 
 export default function DashboardPage() {
     return (
-        <div className="flex flex-col md:flex-row w-full gap-4">
+        <div className="flex flex-col md:flex-row w-full">
             <TodosCard className="w-full xl:w-1/3 order-2 md:order-none" limit={5} />
         </div>
     )
