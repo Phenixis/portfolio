9db37ef0aa5519c968969de6c30da4f5653640e5
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-04-07 21:32:54 +0100

    refactor: add header to DashboardPage and adjust Calendar component styling

--- app/(back-office)/(inside)/my/page.tsx
+++ app/(back-office)/(inside)/my/page.tsx
@@ -4,8 +4,11 @@ import Calendar from "@/components/big/calendar/calendar"
 export default function DashboardPage() {
     return (
         <div className="flex flex-col md:flex-row w-full">
+            <h1 className="text-3xl text-center p-2 md:hidden">
+                Dashboard
+            </h1>
             <TodosCard className="w-full xl:w-1/3 order-2 md:order-none" limit={5} />
-            <Calendar />
+            <Calendar className="order-1 md:order-none"/>
         </div>
     )
 }
