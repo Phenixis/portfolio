0e71bd5e5efa4acf253816d4f08fd45f5a95ca64
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-04-07 21:33:01 +0100

    refactor: enhance Calendar component styling and accept className prop

--- components/big/calendar/calendar.tsx
+++ components/big/calendar/calendar.tsx
@@ -10,20 +10,25 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { cn } from "@/lib/utils"
 
 
-export default function Calendar() {
+export default function Calendar({
+    className
+} : {
+    className: string
+}) {
     const [date, setDate] = useState<Date | undefined>(undefined);
 
     return (
-        <div className="w-full md:max-w-2xl flex justify-center">
-            <Card>
+        <div className={cn("w-full h-fit md:max-w-2xl flex justify-center", className)}>
+            <Card className="w-full md:w-fit">
                 <CardHeader>
                     <CardTitle className="text-center">
                         Calendar
                     </CardTitle>
                 </CardHeader>
-                <CardContent>
+                <CardContent className="w-full md:w-fit flex justify-center">
                     <CalendarComponent
                         mode="single"
                         selected={date}
