8623f924e5a3cda173e855ece08f35c31b299309
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-04-25 21:12:00 +0100

    refactor: move calculateUrgency function to a new task utility file for better organization

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,13 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function calculateUrgency(dueDate: Date) {
-  const now = new Date()
-  const diff = dueDate.getTime() - now.getTime()
-  const diffInDays = Math.ceil(diff / (1000 * 60 * 60 * 24))
-
-  const urgency = Math.ceil(5 - (diffInDays / 2))
-
-  return urgency < 1 ? 1 : urgency
-}
