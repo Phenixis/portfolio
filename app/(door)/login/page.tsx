644b939858e4ed5022ee1f938d65c9d4fa219391
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-04-26 11:56:53 +0100

    feat: redesign login page with OTP input for identifier and password, enhancing user experience and security

--- app/(door)/login/page.tsx
+++ app/(door)/login/page.tsx
@@ -1,16 +1,41 @@
 "use client"
 
-import { Input } from "@/components/ui/input"
-import { login } from "@/lib/auth/actions"
+import { Label } from "@/components/ui/label"
+import { verifyCredentials } from "@/lib/auth/actions"
 import { useActionState, useEffect, useState, useRef, startTransition } from "react"
 import type { ActionState } from "@/middleware"
 import { Loader } from "lucide-react"
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSeparator,
+    InputOTPSlot,
+} from "@/components/ui/input-otp"
+import {
+    REGEXP_ONLY_DIGITS
+} from "input-otp"
 
 export default function Login() {
     const [redirectTo, setRedirectTo] = useState("/my")
-    const [trial, setTrial] = useState("")
+    const [identifier, setIdentifier] = useState("")
+    const [password, setPassword] = useState("")
+    const [hiddenPassword, setHiddenPassword] = useState("")
     const formRef = useRef<HTMLFormElement>(null)
-    const [state, formAction, pending] = useActionState<ActionState, FormData>(login, { error: "" })
+    const [state, formAction, pending] = useActionState<ActionState, FormData>((prevState, formData) => {
+        formData.append("redirectTo", redirectTo)
+        formData.append("identifier", identifier)
+        formData.append("password", password)
+        console.log(formData)
+        return verifyCredentials(prevState, formData)
+    }, { error: "" })
 
     useEffect(() => {
         const params = new URLSearchParams(window.location.search)
@@ -39,16 +64,6 @@ export default function Login() {
     }, [formAction])
 
     useEffect(() => {
-        if (trial.length === 4) {
-            startTransition(() => {
-                if (formRef.current) {
-                    formAction(new FormData(formRef.current))
-                }
-            })
-        }
-    }, [trial])
-
-    useEffect(() => {
         if (state?.success) {
             window.location.href = state.redirectTo;
         }
@@ -60,23 +75,75 @@ export default function Login() {
             ref={formRef}
             className="w-full min-h-screen flex flex-col space-y-4 items-center justify-center"
         >
-            <h1 className="text-2xl">Hello</h1>
-            <input type="text" name="redirectTo" className="hidden" value={redirectTo} readOnly />
-            <Input
-                type="password"
-                name="password"
-                className="max-w-16 text-center"
-                value={trial}
-                onChange={(e) => setTrial(e.target.value)}
-                maxLength={4}
-            />
-            {
-                pending ? (
-                    <Loader className="animate-spin size-4" />
-                ) : (
-                    state?.error && <p className="text-red-500">{state.error}</p>
-                )
-            }
+            <Card>
+                <CardHeader>
+                    <CardTitle>Hello</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <input type="text" name="redirectTo" className="hidden" value={redirectTo} readOnly />
+                    <Label>Enter your identifier</Label>
+                    <InputOTP
+                        maxLength={8}
+                        value={identifier}
+                        onChange={(value) => setIdentifier(value)}
+                        pattern={REGEXP_ONLY_DIGITS}
+                    >
+                        <InputOTPGroup>
+                            <InputOTPSlot index={0} />
+                            <InputOTPSlot index={1} />
+                            <InputOTPSlot index={2} />
+                            <InputOTPSlot index={3} />
+                            <InputOTPSlot index={4} />
+                            <InputOTPSlot index={5} />
+                            <InputOTPSlot index={6} />
+                            <InputOTPSlot index={7} />
+                        </InputOTPGroup>
+                    </InputOTP>
+                    <Label>Enter your password</Label>
+                    <InputOTP
+                        type="password"
+                        maxLength={8}
+                        value={hiddenPassword}
+                        onChange={(value) => {
+                            if (value.length > password.length) {
+                                const realValue = password + value.slice(-1)
+                                const regex = /^\d*$/
+                                if (regex.test(realValue)) {
+                                    setPassword(realValue)
+                                    setHiddenPassword("*".repeat(realValue.length))
+                                }
+                            } else {
+                                setPassword(password.slice(0, value.length))
+                                setHiddenPassword(value)
+                            }
+                        }}
+                    >
+                        <InputOTPGroup>
+                            <InputOTPSlot index={0} />
+                            <InputOTPSlot index={1} />
+                            <InputOTPSlot index={2} />
+                            <InputOTPSlot index={3} />
+                            <InputOTPSlot index={4} />
+                            <InputOTPSlot index={5} />
+                            <InputOTPSlot index={6} />
+                            <InputOTPSlot index={7} />
+                        </InputOTPGroup>
+                    </InputOTP>
+                </CardContent>
+                <CardFooter className={`${pending ? "flex justify-between" : "flex justify-end"}`}>
+                    {
+                        pending ? (
+                            <Loader className="animate-spin size-4" />
+                        ) : (
+                            state?.error && <p className="text-red-500">{state.error}</p>
+                        )
+                    }
+                    <Button
+                        type="submit"
+                        disabled={pending}
+                    >Login</Button>
+                </CardFooter>
+            </Card>
         </form>
     )
 }
