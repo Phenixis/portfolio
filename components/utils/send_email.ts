cd0ff6c86f1dd9fcbb6483102f1c35d10a24efb0
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-04-26 09:47:21 +0100

    feat: add sendWelcomeEmail function to send personalized welcome emails to new users

--- components/utils/send_email.ts
+++ components/utils/send_email.ts
@@ -1,3 +1,4 @@
+import { User } from '@/lib/db/schema';
 import axios from 'axios';
 
 export async function sendEmail(to: string, subject: string, htmlContent: string) {
@@ -30,4 +31,93 @@ export async function sendEmail(to: string, subject: string, htmlContent: string
         console.error('Error sending email:', error);
         throw error;
     }
+}
+
+export async function sendWelcomeEmail(user: User, password: string) {
+    let emailContent = `
+            <html>
+            <head>
+            <style>
+            body {
+                background-color: #f3f4f6;
+                margin: 0;
+                font-family: Arial, sans-serif;
+            }
+            main {
+                max-width: 768px;
+                width: 100%;
+                margin: 20px auto;
+                padding: 20px;
+                background-color: #ffffff;
+                border-radius: 8px;
+                box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            }
+            h1 {
+                font-size: 1.5rem;
+                font-weight: bold;
+                color: #000000;
+                margin-bottom: 16px;
+                text-align: center;
+            }
+            h2 {
+                font-size: 1.25rem;
+                font-weight: 600;
+                color: #374151;
+                margin-top: 16px;
+            }
+            h3 {
+                font-size: 1.125rem;
+                font-weight: 500;
+                color: #4b5563;
+                margin-top: 8px;
+            }
+            ul {
+                list-style-type: disc;
+                padding-left: 20px;
+                margin: 0;
+            }
+            li {
+                color: #1f2937;
+                margin-bottom: 8px;
+            }
+            @media (max-width: 600px) {
+                main {
+                    padding: 10px;
+                }
+                h1 {
+                    font-size: 1.25rem;
+                }
+                h2 {
+                    font-size: 1rem;
+                }
+                h3 {
+                    font-size: 0.875rem;
+                }
+                ul {
+                    padding-left: 15px;
+                }
+                li {
+                    font-size: 0.875rem;
+                }
+            }
+            </style>
+            </head>
+            <body>
+            <main>
+            <h1>Welcome</h1>
+            <h2>Your account has been created, here are your credentials</h2>
+            <p>These credentials are unique and this is the only time you will see them, please save them in a secure location.</p>
+            <p>Identifier: ${user.id}</p>
+            <p>Password: ${password}</p>
+            <p>You can now login to the app <a href="${process.env.NEXT_PUBLIC_APP_URL}/login">here</a></p>
+            <p>If you have any questions, please contact us at <a href="mailto:max@maximeduhamel.com">max@maximeduhamel.com</a></p>
+            <p>Thank you again for your trust.</p>
+            <p>Best regards,</p>
+            <p>Maxime Duhamel</p>
+            </main>
+            </body>
+            </html>
+        `;
+
+    await sendEmail(user.email, "Welcome to the app", emailContent);
 }
