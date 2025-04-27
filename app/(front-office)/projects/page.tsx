3075b8555d0bf22a5b2f93625e9d8f2301efe179
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-04-27 13:40:06 +0100

    refactor: update projects page to use Project component and improve content structure

--- app/(front-office)/projects/page.tsx
+++ app/(front-office)/projects/page.tsx
@@ -9,7 +9,9 @@ import {
     CollapsibleTrigger,
 } from "@/components/ui/collapsible"
 import { ChevronDown } from "lucide-react"
-import Projects from "@/components/big/projects"
+import Project, { colorVariants, states } from "@/components/big/project"
+import projects from "./projects.json"
+
 
 export default function Page() {
     return (
@@ -18,7 +20,7 @@ export default function Page() {
                 My Projects
             </h1>
             <Collapsible className="group/collapsible data-[state=open]:bg-gray-100 dark:data-[state=open]:bg-gray-900 border-transparent dark:lg:hover:bg-gray-900 lg:hover:bg-gray-100 rounded-md p-2">
-                <CollapsibleTrigger className="flex justify-between gap-4">
+                <CollapsibleTrigger className="flex justify-between items-center gap-4">
                     <p className="text-left">
                         I build web applications since Septembre 2024, but my journey started a long time ago...
                     </p>
@@ -29,15 +31,15 @@ export default function Page() {
                         My first ever "technological invention" was born in 2021. I was using the  Google Drive application to synchronize a large number of files (1000~2000) between my computer and my phone.
                     </p>
                     <p>
-                        With such number of files, errors happen and Google Drive displayed them one by one for each file. There was no option for ignoring every errors, so it was mandatory to delete them one. by. one.
+                        With such number of files, errors happenned often and Google Drive displayed them one by one for each file. There was no option for ignoring every errors, so I had to delete them one. by. one.
                     </p>
                     <p>
-                        As I was learning how to code in Python, I decided that I would build a small program that would make it for me. There was no API so I needed to use the graphical interface. I searched for a library to move the mouse to click on the 3 dots button, click on "Delete" and start again for the next error.
+                        As I was learning how to code in Python, I decided that I would build a small program that would do it for me. There was no API so I used the graphical interface. I searched for a library to move the mouse to click on the 3 dots button, click on "Delete" and start again for the next error.
                     </p>
                     <p>
-                        It took me probably a full afternoon to build the program from A to Z, but I had a problem, so I built something to solve it. Since then, the
+                        It took me probably a full afternoon to build the program from A to Z, but I had a problem, and I built something to solve it. Since then, the
                         <Tooltip>
-                            <TooltipTrigger className="underline cursor-help">
+                            <TooltipTrigger className=" inline-block underline cursor-default">
                                 producer mindset
                             </TooltipTrigger>
                             <TooltipContent>
@@ -45,9 +47,16 @@ export default function Page() {
                             </TooltipContent>
                         </Tooltip> has a little place in a corner of my mind.
                     </p>
+                    <p>
+                        That's why to this day, I build my own solutions to my problems, and here are the solutions I built.
+                    </p>
                 </CollapsibleContent>
             </Collapsible>
-            <Projects />
+            <div className="grid grid-cols-2 gap-2">
+                {Object.entries(projects).map(([key, value]) => (
+                    <Project key={key} name={value.name} description={value.description} color={value.color as keyof typeof colorVariants} state={value.state as typeof states[number]} />
+                ))}
+            </div>
         </section>
     )
 }
