d134028e3db722b6793c1abaf48caaa0655a19f2
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-01 15:15:32 +0100

    refactor: update Calendar component layout for improved responsiveness and readability

--- components/big/calendar/calendar.tsx
+++ components/big/calendar/calendar.tsx
@@ -2,14 +2,6 @@
 
 import { useState } from "react";
 import { Calendar as CalendarComponent } from "@/components/ui/calendar";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
 import { cn } from "@/lib/utils"
 
 
@@ -23,7 +15,7 @@ export default function Calendar({
     );
 
     return (
-        <div className={cn("flex flex-col justify-start items-center border border-gray-100 lg:h-screen", className)}>
+        <div className={cn("flex flex-row lg:flex-col justify-start items-start lg:items-center border border-gray-100 dark:border-gray-800 lg:h-screen", className)}>
             <CalendarComponent
                 mode="single"
                 selected={date}
@@ -32,38 +24,44 @@ export default function Calendar({
                     console.log(day);
                 }}
             />
-            <div className="flex items-center justify-center w-full text-2xl">
-                <div className="flex flex-col items-center justify-center w-fit text-xl p-2">
-                    {date?.getDate()}
-                </div>
-                <div className="flex flex-col items-center justify-center w-full text-xl">
-                    <div className="w-full">
-                        {date?.toLocaleDateString(
-                            undefined,
-                            {
-                                weekday: "short",
-                            }
-                        )}
+            <div>
+                <div className="flex items-center justify-center w-full text-2xl">
+                    <div className="flex flex-col items-center justify-center w-fit text-xl p-2">
+                        {date?.getDate()}
                     </div>
-                    <div className="w-full text-base flex flex-row gap-2">
-                        <div>
+                    <div className="flex flex-col items-center justify-center w-full text-xl">
+                        <div className="w-full">
                             {date?.toLocaleDateString(
                                 undefined,
                                 {
-                                    month: "long",
-                                    year: "numeric"
+                                    weekday: "short",
                                 }
                             )}
                         </div>
-                        <div>
-                            {(() => {
-                                if (!date) return '';
-                                const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-                                d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-                                const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-                                const weekNumber = Math.ceil((((d.getTime() - yearStart.getTime()) / 86400000) + 1) / 7);
-                                return `Week ${weekNumber}`;
-                            })()}
+                        <div className="w-full text-base flex flex-row gap-2">
+                            <div>
+                                {date?.toLocaleDateString(
+                                    undefined,
+                                    {
+                                        month: "long",
+                                        year: "numeric"
+                                    }
+                                )}
+                            </div>
+                            <div>
+                                {(() => {
+                                    if (!date) return '';
+                                    const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+                                    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+                                    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+                                    const weekNumber = Math.ceil((((d.getTime() - yearStart.getTime()) / 86400000) + 1) / 7);
+                                    return (
+                                        <span>
+                                            W<span className="hidden lg:inline">eek </span>{weekNumber}
+                                        </span>
+                                    )
+                                })()}
+                            </div>
                         </div>
                     </div>
                 </div>
