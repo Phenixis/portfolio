d5dd70b7fa05c01fefbaf316a5cb5827e6f74837
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-05 11:10:44 +0100

    feat: implement CRUD operations for notes in the database

---
+++ lib/db/queries/note.ts
@@ -0,0 +1,56 @@
+"use server"
+
+import {
+    desc,
+    asc,
+    eq,
+    isNull,
+    isNotNull,
+    sql,
+    and,
+    or,
+    lte,
+    inArray,
+    not
+} from "drizzle-orm"
+import { db } from "../drizzle"
+import * as Schema from "../schema"
+
+export async function getNotes(userId: string, title?: string, projectTitle?: string, limit: number = 50) {
+    const notes = await db.select().from(Schema.note).where(
+        and(
+            eq(Schema.note.user_id, userId),
+            title ? eq(Schema.note.title, title) : undefined,
+            projectTitle ? eq(Schema.note.project_title, projectTitle) : undefined
+        )
+    ).limit(limit)
+
+    return notes
+}
+
+export async function createNote(userId: string, title: string, content: string, projectTitle?: string) {
+    const note = await db.insert(Schema.note).values({
+        user_id: userId,
+        title,
+        content,
+        project_title: projectTitle
+    })
+
+    return note
+}
+
+export async function updateNote(userId: string, id: number, title: string, content: string, projectTitle?: string) {
+    const note = await db.update(Schema.note).set({
+        title,
+        content,
+        project_title: projectTitle
+    }).where(and(eq(Schema.note.id, id), eq(Schema.note.user_id, userId)))
+
+    return note
+}
+
+export async function deleteNote(userId: string, id: number) {
+    const note = await db.delete(Schema.note).where(and(eq(Schema.note.id, id), eq(Schema.note.user_id, userId)))
+
+    return note
+}
