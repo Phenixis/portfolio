9e26603f654dec945f6c97e76bce36673cd413c4
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-05 18:50:17 +0100

    refactor: update getNotes to order by ascending title and modify createNote and updateNote to accept salt and iv parameters

--- lib/db/queries/note.ts
+++ lib/db/queries/note.ts
@@ -24,27 +24,31 @@ export async function getNotes(userId: string, title?: string, projectTitle?: st
             title ? eq(Schema.note.title, title) : undefined,
             projectTitle ? eq(Schema.note.project_title, projectTitle) : undefined
         )
-    ).orderBy(desc(Schema.note.title)).limit(limit)
+    ).orderBy(asc(Schema.note.title)).limit(limit)
 
     return notes
 }
 
-export async function createNote(userId: string, title: string, content: string, projectTitle?: string) {
+export async function createNote(userId: string, title: string, content: string, projectTitle?: string, salt?: string, iv?: string) {
     const note = await db.insert(Schema.note).values({
         user_id: userId,
         title,
         content,
-        project_title: projectTitle
+        project_title: projectTitle,
+        salt,
+        iv
     })
 
     return note
 }
 
-export async function updateNote(userId: string, id: number, title: string, content: string, projectTitle?: string) {
+export async function updateNote(userId: string, id: number, title: string, content: string, projectTitle?: string, salt?: string, iv?: string) {
     const note = await db.update(Schema.note).set({
         title,
         content,
-        project_title: projectTitle
+        project_title: projectTitle,
+        salt,
+        iv
     }).where(and(eq(Schema.note.id, id), eq(Schema.note.user_id, userId)))
 
     return note
