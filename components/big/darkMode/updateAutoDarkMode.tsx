889646a2aa7b76535ba603d5cc8cf1531633a553
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-10 21:53:16 +0100

    refactor: simplify dark mode cookie updates with debounced callback for improved performance

--- components/big/darkMode/updateAutoDarkMode.tsx
+++ components/big/darkMode/updateAutoDarkMode.tsx
@@ -15,6 +15,7 @@ import { useState, useEffect } from 'react'
 import { updateDarkModeCookie } from '@/lib/cookies'
 import { getUser } from '@/lib/auth/session'
 import { updateDarkModePreferences } from '@/lib/db/queries/user'
+import { useDebouncedCallback } from 'use-debounce'
 
 export default function UpdateAutoDarkMode({
     darkModeCookie
@@ -25,28 +26,35 @@ export default function UpdateAutoDarkMode({
     const [startTime, setStartTime] = useState((darkModeCookie.startHour < 10 ? "0" + darkModeCookie.startHour : darkModeCookie.startHour) + ":" + (darkModeCookie.startMinute < 10 ? "0" + darkModeCookie.startMinute : darkModeCookie.startMinute) + ":00")
     const [endTime, setEndTime] = useState((darkModeCookie.endHour < 10 ? "0" + darkModeCookie.endHour : darkModeCookie.endHour) + ":" + (darkModeCookie.endMinute < 10 ? "0" + darkModeCookie.endMinute : darkModeCookie.endMinute) + ":00")
 
+    const updateCookie = useDebouncedCallback(async () => {
+        const newCookie = {
+            ...darkModeCookie,
+            startHour: parseInt(startTime.split(':')[0]),
+            startMinute: parseInt(startTime.split(':')[1]),
+            endHour: parseInt(endTime.split(':')[0]),
+            endMinute: parseInt(endTime.split(':')[1]),
+            auto_dark_mode: autoDarkMode,
+        }
+
+        await updateDarkModeCookie(newCookie)
+        const user = await getUser()
+        if (user) {
+            await updateDarkModePreferences({
+                userId: user.id,
+                darkModeCookie: newCookie
+            })
+        }
+    }, 1000)
+
     return (
         <Card className="w-fit">
             <CardHeader className={`flex flex-row items-center gap-2 space-y-0 ${!autoDarkMode && "xl:pb-6"}`}>
                 <Checkbox
                     id="auto-dark-mode"
                     defaultChecked={autoDarkMode}
-                    onCheckedChange={async () => {
+                    onCheckedChange={() => {
                         setAutoDarkMode(!autoDarkMode)
-
-                        const newCookie = {
-                            ...darkModeCookie,
-                            auto_dark_mode: !autoDarkMode
-                        }
-
-                        await updateDarkModeCookie(newCookie)
-                        const user = await getUser()
-                        if (user) {
-                            await updateDarkModePreferences({
-                                userId: user.id,
-                                darkModeCookie: newCookie
-                            })
-                        }
+                        updateCookie()
                     }}
                 />
                 <CardTitle className="text-base xl:text-lg">Auto Dark Mode</CardTitle>
@@ -57,48 +65,18 @@ export default function UpdateAutoDarkMode({
                         <Input
                             type="time"
                             defaultValue={startTime}
-                            onChange={async (e) => {
+                            onChange={(e) => {
                                 setStartTime(e.target.value)
-                                const [hours, minutes, seconds] = e.target.value.split(':')
-
-                                const newCookie = {
-                                    ...darkModeCookie,
-                                    startHour: parseInt(hours),
-                                    startMinute: parseInt(minutes)
-                                }
-
-                                await updateDarkModeCookie(newCookie)
-                                const user = await getUser()
-                                if (user) {
-                                    await updateDarkModePreferences({
-                                        userId: user.id,
-                                        darkModeCookie: newCookie
-                                    })
-                                }
+                                updateCookie()
                             }}
                         />
                         <Label className="text-base xl:text-lg">to</Label>
                         <Input
                             type="time"
                             defaultValue={endTime}
-                            onChange={async (e) => {
+                            onChange={(e) => {
                                 setEndTime(e.target.value)
-                                const [hours, minutes, seconds] = e.target.value.split(':')
-
-                                const newCookie = {
-                                    ...darkModeCookie,
-                                    endHour: parseInt(hours),
-                                    endMinute: parseInt(minutes)
-                                }
-
-                                await updateDarkModeCookie(newCookie)
-                                const user = await getUser()
-                                if (user) {
-                                    await updateDarkModePreferences({
-                                        userId: user.id,
-                                        darkModeCookie: newCookie
-                                    })
-                                }
+                                updateCookie()
                             }}
                         />
                     </CardContent>
