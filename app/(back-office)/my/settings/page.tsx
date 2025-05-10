f8800290406629c3c9cf993136e9c74a095f7937
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-10 18:05:26 +0100

    feat: integrate UpdateAutoDarkMode component and fetch dark mode cookie in SettingsPage

--- app/(back-office)/my/settings/page.tsx
+++ app/(back-office)/my/settings/page.tsx
@@ -1,9 +1,12 @@
+import UpdateAutoDarkMode from "@/components/big/darkMode/updateAutoDarkMode"
+import { getDarkModeCookie } from "@/lib/cookies"
 
-
-export default function SettingsPage() {
+export default async function SettingsPage() {
+    const darkModeCookie = await getDarkModeCookie()
     return (
         <div>
             <h1>Settings</h1>
+            <UpdateAutoDarkMode darkModeCookie={darkModeCookie} />
         </div>
     )
 }
