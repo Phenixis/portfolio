4d3625ebf346fd4e322109fac5f768a1f07ef0a2
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-11 19:56:39 +0100

    feat: add useNumberOfTasks hook for fetching task data with date range

---
+++ hooks/use-daily-moods.ts
@@ -0,0 +1,31 @@
+"use client"
+
+import type { DailyMood, Task, TaskWithRelations } from "@/lib/db/schema"
+import { useFilteredData } from "./use-filtered-data"
+
+// hooks/use--number-of-tasks.ts
+export function useNumberOfTasks({
+    enabled,
+    startDate,
+    endDate,
+}: {
+    startDate: Date
+    endDate: Date
+    enabled?: boolean
+}) {
+    const { data, isLoading, isError, mutate } = useFilteredData<DailyMood[]>({
+        endpoint: "/api/mood",
+        params: {
+            startDate: new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()).toISOString(),
+            endDate: new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate()).toISOString(),
+        },
+        skipFetch: !enabled,
+    })
+
+    return {
+        data: data || [],
+        isLoading,
+        isError,
+        mutate,
+    }
+}
