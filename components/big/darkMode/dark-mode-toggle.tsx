4b076cf5b2d9f8b01fa79fd96ef615edf9199710
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-11 21:35:37 +0100

    fix: synchronize document class with user preferences for dark mode

--- components/big/darkMode/dark-mode-toggle.tsx
+++ components/big/darkMode/dark-mode-toggle.tsx
@@ -48,6 +48,7 @@ export default function DarkModeToggle({
             if (userPreferences) {
                 setCookie(userPreferences as DarkModeCookie)
                 setIsDarkMode(userPreferences.dark_mode)
+                document.documentElement.classList.toggle("dark", userPreferences.dark_mode)
             }
         })
 
