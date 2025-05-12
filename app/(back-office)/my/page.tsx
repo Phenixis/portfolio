74cd31abb8fa66ad975b52735623011fba74c9d3
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-12 20:07:12 +0100

    fix: enhance layout by adding height to main container and child elements

--- app/(back-office)/my/page.tsx
+++ app/(back-office)/my/page.tsx
@@ -4,11 +4,11 @@ import { NotesCard } from "@/components/big/notes/notes-card"
 
 export default async function DashboardPage() {
     return (
-        <div className="flex flex-col md:flex-row md:justify-between w-full">
+        <div className="flex flex-col md:flex-row md:justify-between w-full h-full">
             <h1 className="text-3xl text-center p-2 md:hidden">
                 Dashboard
             </h1>
-            <div className="flex flex-col md:order-none order-2 w-full lg:w-1/3 lg:max-h-screen">
+            <div className="flex flex-col order-2 md:order-none md:max-h-screen w-full h-full lg:w-1/3">
                 <TasksCard className="w-full lg:m-4" limit={5} />
                 <NotesCard className="w-full lg:m-4" limit={5} />
             </div>
