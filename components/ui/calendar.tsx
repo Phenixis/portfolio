31d1486870588e3044d9a52cd4e602d5266d8be6
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-12 20:07:40 +0100

    fix: increase cell height in Calendar component for improved layout

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -98,10 +98,10 @@ function Calendar({
                 head_row: "flex",
                 head_cell: "text-muted-foreground rounded-md w-7 lg:w-9 font-normal text-[0.8rem]",
                 row: "flex w-full mt-2",
-                cell: "h-7 lg:h-9 w-7 lg:w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+                cell: "h-10 lg:h-9 w-7 lg:w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
                 day: cn(
                     buttonVariants({ variant: "ghost" }),
-                    "h-7 lg:h-9 w-7 lg:w-9 p-0 font-normal aria-selected:opacity-100 relative",
+                    "h-10 lg:h-9 w-7 lg:w-9 p-0 font-normal aria-selected:opacity-100 relative",
                 ),
                 day_range_end: "day-range-end",
                 day_selected:
