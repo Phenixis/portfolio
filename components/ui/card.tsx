9f1d4791f9a57f19f5781cc59dbab1e521667c4f
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-15 13:17:20 +0100

    feat: add relative positioning to CardHeader for improved layout

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -23,7 +23,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 px-2 pt-2 pb-0 md:px-4 md:pt-4 md:pb-2 xl:px-6 xl:pt-6", className)}
+    className={cn("flex relative flex-col space-y-1.5 px-2 pt-2 pb-0 md:px-4 md:pt-4 md:pb-2 xl:px-6 xl:pt-6", className)}
     {...props}
   />
 ))
