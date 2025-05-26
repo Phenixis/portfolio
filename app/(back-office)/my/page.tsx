dea31672c39e9bc8914e1b8af6e632d753b9602a
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-05-26 11:01:23 +0100

    fix: update header text from "Dashboard" to "Life OS" in DashboardPage component

--- app/(back-office)/my/page.tsx
+++ app/(back-office)/my/page.tsx
@@ -9,8 +9,8 @@ export default async function DashboardPage() {
     
     return (
         <div className="flex flex-col md:flex-row md:justify-between w-full h-full">
-            <h1 className="text-3xl text-center p-2 md:hidden">
-                Dashboard
+            <h1 className="w-full text-3xl text-center p-2 md:hidden">
+                Life OS
             </h1>
             <div className="flex flex-col order-2 md:order-none md:max-h-screen w-full h-full lg:w-1/3">
                 <TasksCard 
